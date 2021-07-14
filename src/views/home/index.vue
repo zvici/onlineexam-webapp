9398fa3d232f47fc16681ffbf6391f8ac5b0a8c2
capoocrush <nha.tran@htgsoft.com.vn>
2021-07-14 11:49:23 +0700

    add overlay loading in home page

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,22 +1,29 @@
 <template>
-  <div>
-    <b-row>
-      <b-col
-        v-for="item in listSchedule"
-        :key="item._id"
-        cols="12"
-        md="6"
-      >
-        <test-schedule :data-schedule="item" />
-      </b-col>
-    </b-row>
-  </div>
+  <b-overlay
+    :show="show"
+    rounded="sm"
+    class="overlay-page"
+    spinner-variant="primary"
+  >
+    <div>
+      <b-row>
+        <b-col
+          v-for="item in listSchedule"
+          :key="item._id"
+          cols="12"
+          md="6"
+        >
+          <test-schedule :data-schedule="item" />
+        </b-col>
+      </b-row>
+    </div>
+  </b-overlay>
 </template>
 
 <script>
 import TestSchedule from '@/components/Card/TestSchedule.vue'
 import scheduleApi from '@/api/schedulesApi'
-import { BRow, BCol } from 'bootstrap-vue'
+import { BRow, BCol, BOverlay } from 'bootstrap-vue'
 import { mapGetters } from 'vuex'
 
 export default {
@@ -24,16 +31,16 @@ export default {
     TestSchedule,
     BRow,
     BCol,
+    BOverlay,
   },
   data() {
     return {
       listSchedule: [],
+      show: true,
     }
   },
   computed: {
-    ...mapGetters([
-      'userData',
-    ]),
+    ...mapGetters(['userData']),
   },
   async created() {
     try {
@@ -45,9 +52,15 @@ export default {
     } catch (error) {
       // eslint-disable-next-line no-alert
       alert(error)
+    } finally {
+      this.show = false
     }
   },
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.overlay-page {
+  height: calc(100vh - 120px);
+}
+</style>
