dc5aa80bde2f407be235b443632c18a2ebeb0ef8
capoocrush <nha.tran@htgsoft.com.vn>
2021-07-29 15:34:26 +0700

    add copyright

--- src/@core/layouts/components/AppFooter.vue
+++ src/@core/layouts/components/AppFooter.vue
@@ -1,16 +1,18 @@
 <template>
   <p class="clearfix mb-0">
-    <!-- <span class="float-md-left d-block d-md-inline-block mt-25">
-      COPYRIGHT  © {{ new Date().getFullYear() }}
-      <b-link
+    <span class="float-md-left d-block d-md-inline-block mt-25">
+      COPYRIGHT © {{ new Date().getFullYear() }}
+      <!-- <b-link
         class="ml-25"
         href="https://1.envato.market/pixinvent_portfolio"
         target="_blank"
-      >Pixinvent</b-link>
-      <span class="d-none d-sm-inline-block">, All rights Reserved</span>
+      >Pixinvent</b-link> -->
+      <span class="d-none d-sm-inline-block">Online Exam</span>
     </span>
-
-    <span class="float-md-right d-none d-md-block">Hand-crafted &amp; Made with
+    <!--
+    <span
+      class="float-md-right d-none d-md-block"
+    >Hand-crafted &amp; Made with
       <feather-icon
         icon="HeartIcon"
         size="21"
